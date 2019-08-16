6cf73e68e037cfc9e99ac89c8ff5288239bcfae1
Renato Samperio <renatosamperio@gmail.com>
2019-08-16 18:46:00 +0200

    download_torrent: updated set up configuration trigger

--- src/download_torrent.py
+++ src/download_torrent.py
@@ -103,44 +103,52 @@ class TorrentDownloader(object):
         except Exception as inst:
               ros_node.ParseException(inst)
 
-    def set_up_configuration(self, torrent_info=None): 
+    def set_up_configuration(self, torrent=None): 
         try:
-            if torrent_info is None:
+            if torrent is None:
                 rospy.logwarn ('Setting up torrent failed as no torrent info was provided')
                 ## TODO: Send it to error state?
+                self.fail()
                 return
             
+            ## FSM transition status
             rospy.logdebug('---> Setting up configuration ['+self.previous_state+'] -> ['+self.state+']')
-            #link = 'magnet:?xt=urn:btih:2a1ce38fd6f061e928eb61f492066a5994a9fd0f&dn=Aquaman.2018.DVDR.NTSC.YG&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969'
-            #link = 'magnet:?xt=urn:btih:5D4CC6441554B218E2C5C966C02B0134FCF23B1C&amp;dn=Aquaman+%282018%29+%5B3D%5D+%5BYTS.LT%5D'
             
-            #link = "magnet:?xt=urn:btih:2EAB6D69E8206C982EC29F4E88B5AF83A4E7EAC2&dn=Aquaman+%282018%29&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftorrent.gresille.org%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com:%3A1337%2Fannounce"
-            #print "===> link:", link
-            #print "===> params:", self.params
-            #print torrent_info
-            
-            ## TODO: Set up multiple torrents at the same time
-            link = torrent_info[0]['magnet']
-            
-            ## Preparting torrent downloading
-            self.handle = lt.add_magnet_uri(self.ses, link, self.params)
-            
-            rospy.loginfo('     Downloading metadata...')
-            while (not self.handle.has_metadata() and self.close_now): 
-                time.sleep(0.5)
-            rospy.logdebug('     Got metadata, starting torrent download...')
-            
-            ## Updating torrent downloader state
-            self.status = self.handle.status()
-            
-            ## Setting up connection options
-            self.handle.set_upload_limit(self.up_limit)
-            self.handle.set_max_connections(self.max_connections)
-            
+            ## Looking for selected torrents
+            for torrent_info in torrent['torrent_items']:
+                rospy.logdebug('Setting up [%s] to download'%torrent_info['title_long'])
+                for torrent_data in torrent_info['torrents']:
+                    if torrent_data['state']['status'] == 'selected':
+
+                        ## Preparing to download torrent
+                        torernt_name = torrent_info['title_long']+'-'+torrent_data['quality']+'-'+torrent_data['type']
+                        rospy.loginfo('  Preparing to download torrent [%s]'%(torernt_name))
+                        handle = lt.add_magnet_uri(self.ses, torrent_data['magnet'], self.params) 
+                        
+                        rospy.loginfo('     Downloading metadata...')
+                        start_time = time.time()
+                        metadata_ready = True
+                        metadata_expired = True
+                        while (not handle.has_metadata() and self.close_now and metadata_expired): 
+                            time.sleep(0.5)
+                            metadata_expired = (time.time() - start_time) <= 60
+                            #print "===> metadata_expired:", metadata_expired, self.handle.has_metadata()
+                            
+                        if not handle.has_metadata():
+                            rospy.logwarn('  Meta data not available for [%s]'%torernt_name)
+                        else:
+                            rospy.loginfo('  Got metadata for [%s], starting torrent download...'%torernt_name)
+                        
+                        ## Setting up connection options
+                        handle.set_upload_limit(self.up_limit)
+                        handle.set_max_connections(self.max_connections)
+                        
             ## Moving to next state
-            self.download(torrent_info=torrent_info)
+            self.download(torrent_info=torrent['torrent_items'])
+            
         except Exception as inst:
-              ros_node.ParseException(inst)
+            ros_node.ParseException(inst)
+            self.fail()
               
     def start_downloading(self, torrent_info=None): 
         try:
